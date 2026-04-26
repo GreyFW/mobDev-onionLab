151c8ab89fa18b72890d40c38a83cb6f1b643fb1
GreyF <greysforgettea@gmail.com>
2026-04-26 15:23:57 +0700

    name App

--- app/src/main/java/com/example/todo2/Application.kt
+++ app/src/main/java/com/example/todo2/Application.kt
@@ -1,4 +1,27 @@
 package com.example.todo2
 
-class Application {
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import io.appmetrica.analytics.AppMetrica
+import io.appmetrica.analytics.AppMetricaConfig
+
+@HiltAndroidApp
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initAppMetrica()
+    }
+
+    private fun initAppMetrica() {
+        val config = AppMetricaConfig
+            .newConfigBuilder(BuildConfig.APPMETRICA_API_KEY)
+            .withLogs()
+            .withSessionTimeout(60)
+            .build()
+
+        AppMetrica.activate(this, config)
+
+        AppMetrica.enableActivityAutoTracking(this)
+    }
 }
